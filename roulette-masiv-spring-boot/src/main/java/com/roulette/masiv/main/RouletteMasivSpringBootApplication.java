81e424eb38eb15dc054634aa832eac0b169f1490
anderson <anderson04211@gmail.com>
2021-03-21 20:55:22 -0500

    Project creation through spring-boot using redis. It contains the necessary dependencies.

---
+++ roulette-masiv-spring-boot/src/main/java/com/roulette/masiv/main/RouletteMasivSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.roulette.masiv.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteMasivSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteMasivSpringBootApplication.class, args);
+	}
+
+}
---
+++ roulette-masiv-spring-boot/src/test/java/com/roulette/masiv/main/RouletteMasivSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roulette.masiv.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteMasivSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
